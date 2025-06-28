f3facc2d9bc6b0410eff8af661a76df1e60638d5
Dima <97662429+Dimas0006@users.noreply.github.com>
2025-06-28 15:44:06 +0300

    The first commit

---
+++ MyTestRepo/MyTestRepo/MyTestRepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTestRepoApp.swift
+//  MyTestRepo
+//
+//  Created by Дима Вакулюк on 28.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct MyTestRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
