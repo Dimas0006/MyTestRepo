46c06ce63bbf843c2e459180ed5d684c981855d7
Dima <97662429+Dimas0006@users.noreply.github.com>
2025-06-28 16:37:58 +0300

    main Branch was change

--- MyTestRepo/MyTestRepo/ContentView.swift
+++ MyTestRepo/MyTestRepo/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("Hello, main branch")
+            Text("some chanched")
         }
         .padding()
     }
